8201d0bcee16051b5cbc69c134c6fcda1657d120
HyoBeom <hyobeom@codemind.co.kr>
2023-05-05 16:34:44 +0900

    fix: file path

--- ADTLib/utils/__init__.py
+++ ADTLib/utils/__init__.py
@@ -180,7 +180,7 @@ def tab_create(Onsets,Filename_,save_dir_, bpm):
                             midi_time[i] = midi_time[i] + 0.25
                             DrumMIDI.addNote(track, channel, midi_pitch[i], midi_time[i], duration, volume)
         
-        with open("drums.mid", "wb") as output_file:
+        with open(os.path.join(save_dir_ if save_dir_ else "", "drums.mid"), "wb") as output_file:
             DrumMIDI.writeFile(output_file)
 
 
